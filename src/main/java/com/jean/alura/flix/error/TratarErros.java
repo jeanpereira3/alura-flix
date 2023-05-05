7c6d867ec4cb535f82a93220029812ed63fc0509
jeanpereira3 <jeansilv13@gmail.com>
2023-05-05 10:49:32 -0300

    regras de negocio e testes

--- src/main/java/com/jean/alura/flix/error/TratarErros.java
+++ src/main/java/com/jean/alura/flix/error/TratarErros.java
@@ -2,8 +2,12 @@ package com.jean.alura.flix.error;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
 
 @RestControllerAdvice
 public class TratarErros {
@@ -11,5 +15,18 @@ public class TratarErros {
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity videoNaoEncontrado(){
         return ResponseEntity.badRequest().body("Video não encontrado");
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity camposInvalidos(MethodArgumentNotValidException e){
+        List<FieldError> error = e.getFieldErrors();
+
+        return ResponseEntity.badRequest().body(error.stream().map(DadosErro::new));
+    }
+
+    private record DadosErro(String erro, String mensagem){
+        public DadosErro(FieldError error){
+            this(error.getField(), error.getDefaultMessage());
+        }
     }
 }
